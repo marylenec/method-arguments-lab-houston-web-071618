364ab7b5217bd6427644e8e5ed352dc44522e884
marylene.chan@gmail.com <marylene.chan@gmail.com>
2018-06-29 12:56:05 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
-def introduction(name)
-  puts "Hi, my name is #{name}.\n"
-end
+# def introduction(name)
+#   puts "Hi, my name is #{name}.\n"
+# end
 
-introduction("Josh")
+# introduction("Josh")
 
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
